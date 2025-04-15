f9cf1c264917a952eb83f3a99cbae5fc1ccd1e7f
dumpling-s <61899994+dumpling-s@users.noreply.github.com>
2025-04-16 00:26:33 +0800

    Update qmix_net.py

--- src/qmix_net.py
+++ src/qmix_net.py
@@ -2,6 +2,31 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as f
 
+
+class AgentWithLLMAndRNN(nn.Module):
+    def __init__(self, llm_model, args, device='cuda'):
+        super(AgentWithLLMAndRNN, self).__init__()
+        self.device = device
+        self.args = args
+
+        self.llm = llm_model.to(device)  # 接收外部传入的 LLM
+        self.llm.train()
+
+        self.embedding_dim = self.llm.config.hidden_size
+
+        self.rnn = nn.GRU(self.embedding_dim, self.args.rnn_hidden_dim, batch_first=True)
+        self.q_head = nn.Linear(self.args.rnn_hidden_dim, self.args.num_actions)
+
+    def forward(self, input_ids, attention_mask, hidden_state):
+        outputs = self.llm(input_ids=input_ids.unsqueeze(0), attention_mask=attention_mask, return_dict=True, output_hidden_states=True)
+        last_hidden = outputs.hidden_states[-1]  # [B, T, D]
+        pooled = last_hidden[:, 0, :]  # [B, D]
+
+        rnn_output, next_hidden = self.rnn(pooled.to(dtype=torch.float32), hidden_state.unsqueeze(0))   # [B, 1, H], [1, B, H]
+        q_value = self.q_head(rnn_output.squeeze(1))  # [B, num_actions]
+
+        return q_value, next_hidden
+        
 class RNN(nn.Module):
     def __init__(self, input_shape, args):
         super(RNN, self).__init__()
