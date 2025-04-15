5f62a2c96109ee915c8aaf11360411a49ccb0483
dumpling-s <61899994+dumpling-s@users.noreply.github.com>
2025-04-16 00:24:58 +0800

    Update agent.py

--- src/agent.py
+++ src/agent.py
@@ -6,26 +6,6 @@ from trl import AutoModelForCausalLMWithValueHead, AutoModelForSeq2SeqLMWithValu
 from torch.distributions import Categorical
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-model_id = ""
-print("數據集asdiv，模型：",model_id)
-model_WithValueHead = AutoModelForCausalLMWithValueHead.from_pretrained(
-    model_id,
-    rope_scaling={
-        "type": "llama3",
-        "factor": 8.0,
-        "low_freq_factor": 1.0,
-        "high_freq_factor": 4.0,
-        "original_max_position_embeddings": 8192
-    },
-    device_map='auto',
-    torch_dtype=torch.float16,
-    max_length=300,
-    eos_token_id=2,
-    pad_token_id=2
-)
-tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
-
-
 class Agents:
     def __init__(self, args):
         self.num_actions = args.num_actions
@@ -37,24 +17,23 @@ class Agents:
         self.args = args
         self.model = model_WithValueHead
 
-    def choose_action(self, obs, agent_num, avail_actions, epsilon, evaluate=False):
+    def choose_action(self, obs, agent_num, action, epsilon, evaluate=False):
         inputs = obs.copy()
         agent_id = torch.zeros(self.num_agents).long()
         agent_id[agent_num] = 1.
         inputs = torch.tensor(inputs).unsqueeze(0)
+        hidden_state = self.policy.eval_hidden[:, agent_num, :]
         if self.args.cuda:
             inputs = inputs.cuda()
+            hidden_state = hidden_state.cuda()
 
+        # 以概率ε选择一个随机动作（探索），以概率1-ε选择当前估计最优的动作（利用）
         attention_mask = torch.tensor([1] * len(inputs)).unsqueeze(0).cuda().to(torch.long)
-        with torch.no_grad():
-            lm_logits, loss, value = model_WithValueHead.forward(input_ids=inputs,
-                                                  attention_mask=attention_mask,
-                                                  )
-
         if np.random.uniform() < epsilon:
             action = torch.randint(0, self.args.action_space, (1,  self.args.num_actions)).unsqueeze(0)
         else:
-            action = torch.argmax(lm_logits, dim=2).unsqueeze(0)
+            action = action
+        return action
 
     def _get_max_episode_len(self, batch):
         terminated = batch['terminated']
